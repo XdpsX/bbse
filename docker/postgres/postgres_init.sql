923fbfb23ba1762304d1996dc140a6a6bcfd511c
XdpsX <dranhata@gmail.com>
2025-01-13 19:51:20 +0700

    init identity service

--- docker/postgres/postgres_init.sql
+++ docker/postgres/postgres_init.sql
@@ -3,3 +3,6 @@ LIMIT = -1;
 
 CREATE DATABASE product WITH OWNER = admin ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION
 LIMIT = -1;
+
+CREATE DATABASE 'identity' WITH OWNER = admin ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TABLESPACE = pg_default CONNECTION
+LIMIT = -1;
